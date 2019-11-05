18b5577432d91a6ca2f7210754d176f9e1dbabe2
Yalun Zhu <3287446+nulayuhz@users.noreply.github.com>
2019-11-05 10:50:38 -0500

    add forRoot method

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { TrimValueAccessor } from './trim-value-accessor';
 
 @NgModule({
   declarations: [ TrimValueAccessor ],
   exports: [ TrimValueAccessor ]
 })
-export class TrimValueAccessorModule { }
+export class TrimValueAccessorModule { 
+  public static forRoot(config): ModuleWithProviders {
+    return {
+      ngModule: TrimValueAccessorModule,
+      providers: [
+        { provide: 'config', useValue: config }
+      ]
+    }
+  }
+}
